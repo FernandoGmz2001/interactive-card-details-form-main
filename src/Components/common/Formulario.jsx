9221ce572da1115f53fe17f242462804508dfcee
FernandoGmz2001 <fernandogmz2001@gmail.com>
2023-01-14 15:06:58 -0600

    Version 0.02

--- src/Components/common/Formulario.jsx
+++ src/Components/common/Formulario.jsx
@@ -1,11 +1,6 @@
 import React, { useState } from "react";
 
-function Formulario({ putName, putNumber, putMonth, number, putYear, putCvc }) {
-  // const [name, setName] = useState("");
-  const [expMonth, setExpMonth] = useState("");
-  const [expYear, setExpYear] = useState("");
-  const [cvc, setCvc] = useState("");
-
+function Formulario({ setTarget, validateInputs, number, error, isBlank }) {
   return (
     <div className="formulario">
       <div className="form-group">
@@ -13,11 +8,16 @@ function Formulario({ putName, putNumber, putMonth, number, putYear, putCvc }) {
         <input
           type="text"
           placeholder="e.g.Jane Appleseed"
-          // onChange={(e) => setName(e.target.value)}
-          onChange={putName}
+          onChange={setTarget}
+          name="name"
           required
           maxLength="35"
+          className={error.name || isBlank.name ? "error" : ""}
         />
+        <span className={error.name ? "error" : "hide"}>
+          Wrong format, letters only
+        </span>
+        <span className={isBlank.name ? "error" : "hide"}>Can't be blank</span>
       </div>
       <div className="form-group">
         <label htmlFor="">CARD NUMBER</label>
@@ -26,50 +26,77 @@ function Formulario({ putName, putNumber, putMonth, number, putYear, putCvc }) {
           placeholder="e.g_ 1234 5678 9123 0000"
           required
           maxLength="19"
+          onChange={setTarget}
+          name="number"
           value={number
             .replace(/\s/g, "")
             .replace(/(\d{4})/g, "$1 ")
             .trim()}
-          onChange={putNumber}
+          className={error.number || isBlank.number ? "error" : ""}
         />
+        <span className={error.number ? "error" : "hide"}>
+          Wrong format, numbers only
+        </span>
+        <span className={isBlank.number ? "error" : "hide"}>
+          Can't be blank
+        </span>
       </div>
       <div className="form-group cardInfo">
         <div className="form-group_date">
           <label htmlFor="">EXP.DATE (MM/YY)</label>
-          <div className="form_group_date-dateInfo">
-            <input
-              type="text"
-              placeholder="MM"
-              onChange={putMonth}
-              required
-              maxLength="2"
-              min="1"
-              max="12"
-            />
-            <input
-              type="text"
-              placeholder="YY"
-              onChange={putYear}
-              required
-              maxLength="2"
-              min="23"
-              max="27"
-            />
+          <div className="form_group_date-dateInfo-container">
+            <div className="form_group_date-dateInfo">
+              <input
+                type="text"
+                placeholder="MM"
+                onChange={setTarget}
+                name="expMonth"
+                required
+                maxLength="2"
+                min="1"
+                max="12"
+                className={error.expMonth || isBlank.expMonth ? "error" : ""}
+              />
+              <input
+                type="text"
+                placeholder="YY"
+                onChange={setTarget}
+                name="expYear"
+                required
+                maxLength="2"
+                min="23"
+                max="27"
+                className={error.expYear || isBlank.expYear ? "error" : ""}
+              />
+            </div>
           </div>
+          <span className={error.expMonth || error.expYear ? "error" : "hide"}>
+            Wrong format
+          </span>
+          <span
+            className={isBlank.expMonth || isBlank.expYear ? "error" : "hide"}
+          >
+            Can't be blank
+          </span>
         </div>
         <div className="form-group_cvc">
           <label htmlFor="">CVC</label>
           <input
             type="text"
             placeholder="e.g. 123"
-            onChange={putCvc}
+            onChange={setTarget}
+            name="cvc"
             required
             maxLength="3"
+            className={error.cvc || isBlank.cvc ? "error" : ""}
           />
+          <span className={isBlank.cvc ? "error" : "hide"}>Can't be blank</span>
         </div>
       </div>
       <div className="form-group">
-        <button className="btnConfirm">Confirm</button>
+        <button className="btnConfirm" onClick={validateInputs}>
+          Confirm
+        </button>
       </div>
     </div>
   );
--- src/Components/views/PayPage.jsx
+++ src/Components/views/PayPage.jsx
@@ -4,48 +4,178 @@ import Formulario from "../common/Formulario";
 import Card from "../common/Card";
 import CardBack from "../common/CardBack";
 import Confirmed from "../common/Confirmed";
+
 function PayPage() {
-  const [name, setName] = useState("");
-  const [number, setNumber] = useState("");
-  const [expMonth, setExpMonth] = useState("");
-  const [expYear, setExpYear] = useState("");
-  const [cvc, setCvc] = useState("");
+  const [value, setValue] = useState({
+    name: "",
+    number: "",
+    expMonth: "",
+    expYear: "",
+    cvc: "",
+  });
 
-  const putName = (e) => {
-    setName(e.target.value);
-  };
-  const putNumber = (e) => {
-    setNumber(e.target.value);
-  };
-  const putMonth = (e) => {
-    setExpMonth(e.target.value);
-  };
-  const putYear = (e) => {
-    setExpYear(e.target.value);
-  };
-  const putCvc = (e) => {
-    setCvc(e.target.value);
+  const [error, setError] = useState({
+    name: null,
+    number: null,
+    expMonth: null,
+    expYear: null,
+    cvc: null,
+  });
+
+  const [isBlank, setIsBlank] = useState({
+    name: false,
+    number: false,
+    expMonth: false,
+    expYear: false,
+    cvc: false,
+  });
+
+  const [isConfirmed, setIsConfirmed] = useState(false);
+
+  const setTarget = (e) => {
+    const { name, value } = e.target;
+    setValue((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
   };
 
-  const isConfirmed = false;
+  const validateInputs = () => {
+    const { name, number, expMonth, expYear, cvc } = value;
+    if (name.trim().length !== 0) {
+      setIsBlank((prev) => {
+        return { ...prev, name: false };
+      });
+      var regex =
+        /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
+      regex.test(name)
+        ? setError((prev) => {
+            return { ...prev, name: false };
+          })
+        : setError((prev) => {
+            return { ...prev, name: true };
+          });
+    } else {
+      setIsBlank((prev) => {
+        return { ...prev, name: true };
+      });
+      setError((prev) => {
+        return { ...prev, name: false };
+      });
+    }
+    if (number.trim().length !== 0) {
+      setIsBlank((prev) => {
+        return { ...prev, number: false };
+      });
+      var regex = /^(\d\s?){15,16}$/;
+      regex.test(number)
+        ? setError((prev) => {
+            return { ...prev, number: false };
+          })
+        : setError((prev) => {
+            return { ...prev, number: true };
+          });
+    } else {
+      setIsBlank((prev) => {
+        return { ...prev, number: true };
+      });
+      setError((prev) => {
+        return { ...prev, number: false };
+      });
+    }
+
+    if (expMonth.trim().length !== 0) {
+      setIsBlank((prev) => {
+        return { ...prev, expMonth: false };
+      });
+      var regex = /^(0[1-9]|1[012])$/;
+      regex.test(expMonth)
+        ? setError((prev) => {
+            return { ...prev, expMonth: false };
+          })
+        : setError((prev) => {
+            return { ...prev, expMonth: true };
+          });
+    } else {
+      setIsBlank((prev) => {
+        return { ...prev, expMonth: true };
+      });
+      setError((prev) => {
+        return { ...prev, expMonth: false };
+      });
+    }
+
+    if (expYear.trim().length !== 0) {
+      setIsBlank((prev) => {
+        return { ...prev, expYear: false };
+      });
+      var regex = /^(2[1-9]|0[012])$/;
+      regex.test(expYear)
+        ? setError((prev) => {
+            return { ...prev, expYear: false };
+          })
+        : setError((prev) => {
+            return { ...prev, expYear: true };
+          });
+    } else {
+      setIsBlank((prev) => {
+        return { ...prev, expYear: true };
+      });
+      setError((prev) => {
+        return { ...prev, expYear: false };
+      });
+    }
+
+    if (cvc.trim().length !== 0) {
+      setIsBlank((prev) => {
+        return { ...prev, cvc: false };
+      });
+      var regex = /^[0-9]{3,4}$/;
+      regex.test(cvc)
+        ? setError((prev) => {
+            return { ...prev, cvc: false };
+          })
+        : setError((prev) => {
+            return { ...prev, cvc: true };
+          });
+    } else {
+      setIsBlank((prev) => {
+        return { ...prev, cvc: true };
+      });
+      setError((prev) => {
+        return { ...prev, cvc: false };
+      });
+    }
+    console.log(error);
+  };
+
+  const confirm = () => {
+    setIsConfirmed(true);
+  };
 
   return (
     <main className="main">
       <div className="left-container">
-        <Card Name={name} Number={number} Month={expMonth} Year={expYear} />
-        <CardBack Cvc={cvc} />
+        <Card
+          Name={value.name}
+          Number={value.number}
+          Month={value.expMonth}
+          Year={value.expYear}
+        />
+        <CardBack Cvc={value.cvc} />
       </div>
       <div className="right-container">
         {isConfirmed ? (
           <Confirmed />
         ) : (
           <Formulario
-            putName={putName}
-            putNumber={putNumber}
-            putMonth={putMonth}
-            number={number}
-            putYear={putYear}
-            putCvc={putCvc}
+            setTarget={setTarget}
+            validateInputs={validateInputs}
+            number={value.number}
+            error={error}
+            isBlank={isBlank}
           />
         )}
       </div>
